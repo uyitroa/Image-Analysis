74c33b21446dd955e606de05334c680842b86be7
Shintaro <30695483+shintaroDesu@users.noreply.github.com>
2018-01-03 18:46:24 +0100

    Update countObjet.py

--- countObjet.py
+++ countObjet.py
@@ -1,19 +1,24 @@
+print "importing module please wait..."
 import numpy
 import pylab
 import mahotas
 
+
 image = raw_input("image name: ")
 img = mahotas.imread(image)
+print " filtering"
+imgf = mahotas.gaussian_filter(img,8)
+print "gaussian filtered now thresholding"
 
-pylab.gray()
-T = mahotas.thresholding.otsu(img)
+if str(imgf.dtype) != 'uint8':
+	imgf = imgf.astype(numpy.uint8)
 
-try:
-	imgf = mahotas.gaussian_filter(img,8)
-except:
-	imgf = img
+	T = mahotas.thresholding.otsu(imgf)
+
+
 labeled, nbr = mahotas.label(imgf > T)
-print nbr
+print "objet : ",nbr
+
 pylab.imshow(labeled)
-pylab.jet()
+
 pylab.show()
