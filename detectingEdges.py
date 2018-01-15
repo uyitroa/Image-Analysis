f69b83d341d8c1174d4417958a61714210cc42ce
Shintaro <30695483+shintaroDesu@users.noreply.github.com>
2018-01-15 22:02:38 +0100

    Add files via upload

---
+++ detectingEdges.py
@@ -0,0 +1,18 @@
+import numpy
+from skimage.io import imread
+from skimage.feature import canny
+import mahotas as mh # for only thresholding
+
+from pylab import imshow, show
+
+name = raw_input("Name file: ")
+image = imread(name)
+
+if len(image.shape) == 3:
+	image = image[:,:,0] # convert to 2d array
+
+T = mh.thresholding.otsu(image)
+
+edges = canny(image, sigma = 1, low_threshold = T - 30, high_threshold = T + 20)
+imshow(edges)
+show()
