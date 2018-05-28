0adfe6c044b43d02c0cc3264c0d59ec316574757
Shintaro <30695483+shintaroDesu@users.noreply.github.com>
2018-05-28 16:39:38 +0200

    Update myDetect.py

--- myDetect.py
+++ myDetect.py
@@ -1,5 +1,6 @@
 import numpy
 import mahotas
+from pylab import imshow, show
 
 def openFile():
 	name = input("file name: ")
@@ -15,10 +16,24 @@ class Detect:
 		nCol = len(self.matrix[0])
 		new = numpy.zeros((nRow, nCol))
 		
-		lastColor = self.matrix[0][0]
+		currentColor = self.matrix[0][0]
 		for row in range(nRow):
 			for column in range(nCol):
-				currentColor = self.matrix[row][column]
-				
-				if not (lastColor - 10 < currentColor and lastColor + 10 > currentColor):
-					#idk yet
+				if row + 1 < nRow:
+					nextColor = self.matrix[row + 1][column]
+					if not (currentColor + 10 > nextColor and currentColor - 10 < nextColor):
+						new[row][column] = 1
+				if column + 1 < nCol:
+					nextColor = self.matrix[row][column + 1]
+					if not (currentColor + 10 > nextColor and currentColor - 10 < nextColor):
+						new[row][column] = 1
+		return new
+def test():
+	myMatrix = openFile()
+	detect = Detect(myMatrix)
+	myMatrix = detect.edge()
+	imshow(myMatrix)
+	show()
+	
+if __name__ == "__main__":
+	test()
